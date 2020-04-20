2ee1e7161b9265b2bcd6c80035e22a810b7afe6e
Ztaal <Ztaal@ZTAAL-DESKTOP>
2020-04-20 22:40:32 +0200

    Added QuizCategory containing the quiz entries for each category.

---
+++ src/widgets/QuizCategory.cpp
@@ -0,0 +1,56 @@
+#include "QuizCategory.hpp"
+
+#include <stdexcept>
+
+#include <QLabel>
+#include <QVBoxLayout>
+
+#include "common/Log.hpp"
+
+#include "widgets/QuizEntry.hpp"
+
+
+MusicQuiz::QuizCategory::QuizCategory(QString name, const std::vector<MusicQuiz::QuizEntry*>& entries, QWidget* parent) :
+	QWidget(parent)
+{
+	/** Sanity Check */
+	if ( name.isEmpty() ) {
+		throw std::runtime_error("Cannot create category without a name.");
+	}
+
+	if ( entries.empty() ) {
+		throw std::runtime_error("Cannot create category with empty list of entries.");
+	}
+
+	/** Set Variables */
+	_name = name;
+	_entries = entries;
+
+	/** Create Widget Layout */
+	createLayout();
+}
+
+void MusicQuiz::QuizCategory::createLayout()
+{
+	/** Layout */
+	QVBoxLayout* mainlayout = new QVBoxLayout;
+
+	/** Category Name */
+	QLabel* label = new QLabel(_name);
+	label->setObjectName("categoryLabel");
+	mainlayout->addWidget(label);
+
+	/** Add Entries */
+	for ( size_t i = 0; i < _entries.size(); ++i ) {
+		mainlayout->addWidget(_entries[i]);
+	}
+
+	/** Set Layout */
+	setLayout(mainlayout);
+}
+
+size_t MusicQuiz::QuizCategory::getSize()
+{
+	//return _entries.size();
+	return 0;
+}
---
+++ src/widgets/QuizCategory.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include <QString>
+#include <QObject>
+#include <QWidget>
+
+
+namespace MusicQuiz {
+	class QuizEntry;
+	class QuizCategory : public QWidget
+	{
+		Q_OBJECT
+	public:
+		/**
+		 * @brief Constructor
+		 *
+		 * @param[in] name The category name.
+		 * @param[in] entries The category entries.
+		 * @param[in] parent The parent widget.
+		 */
+		explicit QuizCategory(QString name, const std::vector<MusicQuiz::QuizEntry*>& entries, QWidget* parent = nullptr);
+
+		/**
+		 * @brief Default Destructor
+		 */
+		virtual ~QuizCategory() = default;
+
+		/**
+		 * @brief Gets the size of the category.
+		 *
+		 * @return The size of the category.
+		 */
+		size_t getSize();
+
+	public slots:
+
+	protected:
+		/**
+		 * @brief Creates the category layout.
+		 */
+		void createLayout();
+
+		/** Variables */
+		QString _name = "";
+
+		std::vector<MusicQuiz::QuizEntry*> _entries;
+	};
+}
