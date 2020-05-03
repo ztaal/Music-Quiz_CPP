320b4ee0295a5141fc2a39ecfebfaec278e188ba
Ztaal <Ztaal@ZTAAL-DESKTOP>
2020-05-03 14:35:37 +0200

    Started work on the team selector.

---
+++ src/gui_tools/GuiUtil/TeamSelector.cpp
@@ -0,0 +1,107 @@
+#include "TeamSelector.hpp"
+
+#include <sstream>
+#include <stdexcept>
+
+#include <QLabel>
+#include <QWidget>
+#include <QCheckBox>
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QStringList>
+#include <QListWidget>
+#include <QPushButton>
+#include <QSpacerItem>
+#include <QMessageBox>
+#include <QListWidgetItem>
+
+#include "common/Log.hpp"
+#include "util/QuizSettings.hpp"
+#include "gui_tools/widgets/QuizSettingsDialog.hpp"
+
+
+MusicQuiz::TeamSelector::TeamSelector(QWidget* parent) :
+	QDialog(parent)
+{
+	/** Set Window Flags */
+	setWindowFlags(windowFlags() | Qt::WindowMaximizeButtonHint | Qt::WindowMinimizeButtonHint);
+
+	/** Set Size */
+	const size_t width = 1200;
+	const size_t height = 600;
+	if ( parent == nullptr ) {
+		resize(width, height);
+	} else {
+		setGeometry(parent->x() + parent->width() / 2 - width / 2, parent->y() + parent->height() / 2 - height / 2, width, height);
+	}
+
+	/** Create Layout */
+	createLayout();
+}
+
+void MusicQuiz::TeamSelector::createLayout()
+{
+	/** Layout */
+	QGridLayout* mainlayout = new QGridLayout;
+	mainlayout->setHorizontalSpacing(10);
+	mainlayout->setVerticalSpacing(5);
+	mainlayout->setColumnStretch(0, 1);
+	mainlayout->setColumnStretch(1, 3);
+
+	/** Label */
+	QLabel* topLabel = new QLabel("Select Quiz");
+	topLabel->setObjectName("selectQuizlabel");
+
+	/** Horizontal Line */
+	QWidget* horizontalLine = new QWidget(this);
+	horizontalLine->setFixedHeight(3);
+	horizontalLine->setStyleSheet("background-color: rgb(0, 0, 0);");
+	horizontalLine->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+	/** Add Widgets */
+	mainlayout->addWidget(topLabel, 0, 0, 1, 3, Qt::AlignCenter);
+	mainlayout->addWidget(horizontalLine, 1, 0, 1, 3);
+
+	/** Set Layout */
+	setLayout(mainlayout);
+}
+
+void MusicQuiz::TeamSelector::teamSelected()
+{
+	/** Sanity Check */
+
+
+	/** Current Index */
+
+
+/*	QString msg = QString::fromStdString("Are you sure you want to select quiz '" + _quizPreviews[currentIndex].quizName + "'.");
+	QMessageBox::StandardButton resBtn = QMessageBox::question(this, "Select Quiz?", msg,
+		QMessageBox::No | QMessageBox::Yes, QMessageBox::Yes);
+
+	if ( resBtn == QMessageBox::Yes ) {
+		emit quizSelectedSignal(currentIndex, _settings);
+	}*/
+}
+
+void MusicQuiz::TeamSelector::quit()
+{
+	/** Send Quit Signal */
+	emit quitSignal();
+
+	/** Call Destructor */
+	close();
+}
+
+void MusicQuiz::TeamSelector::keyPressEvent(QKeyEvent* event)
+{
+	switch ( event->key() )
+	{
+	case Qt::Key_Escape:
+		emit quitSignal();
+		break;
+	default:
+		QWidget::keyPressEvent(event);
+		break;
+	}
+}
---
+++ src/gui_tools/GuiUtil/TeamSelector.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <QObject>
+#include <QWidget>
+#include <QDialog>
+#include <QCheckBox>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QKeyEvent>
+#include <QListWidget>
+
+
+namespace MusicQuiz {
+	class TeamSelector : public QDialog
+	{
+		Q_OBJECT
+	public:
+		/**
+		 * @brief Constructor
+		 *
+		 * @param[in] parent The parent widget.
+		 */
+		explicit TeamSelector(QWidget* parent = nullptr);
+
+		/**
+		 * @brief Destructor
+		 */
+		virtual ~TeamSelector() = default;
+
+	public slots:
+		/**
+		 * @brief Handles the key press events.
+		 *
+		 * @param[in] event The event.
+		 */
+		void keyPressEvent(QKeyEvent* event);
+
+	private slots:
+		/**
+		 * @brief Emits the quiz selected signal.
+		 */
+		void teamSelected();
+
+		/**
+		 * @brief Emits the quit signal.
+		 */
+		void quit();
+
+	signals:
+		void quitSignal();
+		void teamSelectedSignal();
+	protected:
+		/**
+		 * @brief Creates the category layout.
+		 */
+		void createLayout();
+
+		/** Variables */
+		QLineEdit* _authorText = nullptr;
+	};
+}
