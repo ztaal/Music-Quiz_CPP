9483dd3eb8182cdee490d8276bb18f66fed1731e
Stefan Ravn van Overeem <stefan@stefanrvo.dk>
2021-12-12 19:29:37 +0100

    Support new NodeInfo message
    Remove Mac OS build as it it broken for some reason

--- build_scripts/dependencies_windows.sh
+++ build_scripts/dependencies_windows.sh
@@ -4,7 +4,7 @@ set -e
 #Install dependencies
 choco install NuGet.CommandLine -y
 choco install 7zip -y
-nuget install boost -OutputDirectory /c/boost
+nuget install boost -Version 1.72.0 -OutputDirectory /c/boost
 
 curl -LO https://code.qt.io/cgit/qbs/qbs.git/plain/scripts/install-qt.sh;
 ./install-qt.sh   --version ${QT_VERSION} --toolchain win64_msvc2017_64 qtbase qtsvg qtmultimedia qttools
